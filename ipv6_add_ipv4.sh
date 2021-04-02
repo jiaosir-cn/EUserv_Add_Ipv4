da7a01aa9fe4c059bd47524beb79efdc231fe9c9
jiaosir-cn <76861519+jiaosir-cn@users.noreply.github.com>
2021-04-03 01:28:25 +0800

    Update ipv6_add_ipv4.sh

--- ipv6_add_ipv4.sh
+++ ipv6_add_ipv4.sh
@@ -4,7 +4,7 @@ apt update && apt install curl sudo lsb-release iptables -y
 echo "deb http://deb.debian.org/debian $(lsb_release -sc)-backports main" | sudo tee /etc/apt/sources.list.d/backports.list
 apt update
 apt install net-tools iproute2 openresolv dnsutils -y
-apt install wireguard-tools --no-install-recommends -y
+apt install wireguard-tools --no-install-recommends
 
 
 //download wgcf wireguard-go
